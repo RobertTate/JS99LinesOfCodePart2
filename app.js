18df94614a1a770d3e68c5591cb1d02ccbdc9e49
Robert Tate <robertjosephtate@gmail.com>
2018-04-05 17:11:26 -0500

    Added Div with Class Name

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ let button = document.getElementsByTagName('button')[0];
 button.addEventListener('click', function () {
 
      for (let f = 0; f < friends.length; f++) {
+         let div = document.createElement("div");
+         div.className = "friend";
          let h3 = document.createElement("h3");
          let friendsName = document.createTextNode(`${friends[f]}`);
          h3.appendChild(friendsName);
-         document.body.appendChild(h3);
+         div.appendChild(h3);
+         document.body.appendChild(div);
          
         for (let i = 10; i >= 1; i--) {
 
