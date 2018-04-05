d939d67f1f3e9e7098f76f82f138a6ee964e2afa
Robert Tate <robertjosephtate@gmail.com>
2018-04-05 16:53:29 -0500

    Completed Part 2 of Lab

---
+++ app.js
@@ -0,0 +1,36 @@
+
+let friends = ['THOMAS', 'IVES', 'JUSTIN', 'CHRIS', 'KENNY'];
+
+let button = document.getElementsByTagName('button')[0];
+
+
+button.addEventListener('click', function () {
+
+     for (let f = 0; f < friends.length; f++) {
+         let h3 = document.createElement("h3");
+         let friendsName = document.createTextNode(`${friends[f]}`);
+         h3.appendChild(friendsName);
+         document.body.appendChild(h3);
+         
+        for (let i = 10; i >= 1; i--) {
+
+            let p = document.createElement('p');
+            let pTextLast = document.createTextNode(`${(i)} line of code in the file, ${(i)} line of code; ${friends[0]} strikes one out, clears it all out, 0 lines of code in the file`);
+            let pTextFirst = document.createTextNode(`${(i)} lines of code in the file, ${(i)} lines of code; ${friends[0]} strikes one out, clears it all out, ${(i - 1)} lines of code in the file`);
+
+            if (i == 1) {
+                p.appendChild(pTextLast);
+                document.body.appendChild(p);
+            } else {
+                p.appendChild(pTextFirst);
+                document.body.appendChild(p);
+            }
+        }
+
+     };
+})
+
+
+
+
+
